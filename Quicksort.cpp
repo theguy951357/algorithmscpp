7eca0886d708a2f7081e2cb9b52ad091fca88e01
theguy951357 <crispspiceguitar@gmail.com>
2021-12-28 20:59:04 -0500

    initial commit

---
+++ Quicksort.cpp
@@ -0,0 +1,11 @@
+//
+// Created by cblah on 12/28/2021.
+//
+
+#include "Quicksort.h"
+
+Quicksort::Quicksort() {}
+
+Quicksort::~Quicksort() {
+
+}
---
+++ Quicksort.h
@@ -0,0 +1,18 @@
+//
+// Created by cblah on 12/28/2021.
+//
+
+#ifndef QUICKSORT_QUICKSORT_H
+#define QUICKSORT_QUICKSORT_H
+
+
+class Quicksort {
+public:
+    Quicksort();
+
+    virtual ~Quicksort();
+
+};
+
+
+#endif //QUICKSORT_QUICKSORT_H
